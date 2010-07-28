a0dd0bd638cdad92ab08ec4aeb38e140ef9706d5
HaskellElephant <eivind.jahren@gmail.com>
2010-07-28 08:46:24 +0200

    add licence and removed old main.hs

--- Pacman.hs
+++ Pacman.hs
@@ -1,4 +1,20 @@
 {-# OPTIONS -fglasgow-exts #-}
+--    Copyright (C) 2010 Eivind Jahren and Marius Ludvigsen    
+--    This file is part of Hacman.
+--
+--    Hacman is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    Hacman is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 -- | The pacman module gives the Haskell bindings to pacman.
 -- Here package attribute can be a package name, version
 -- downloadSize, url and so on. A package is the collection of such
